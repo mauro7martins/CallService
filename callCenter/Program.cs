d12d1ce85127f85c9efc1600539ad3aedf21f28d
unknown <mauro.martins@N071.programmers.com.br>
2012-08-31 15:28:07 -0300

    fisrt commit

---
+++ callCenter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace callCenter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
